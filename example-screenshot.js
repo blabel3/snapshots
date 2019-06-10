ff889ed11b9b41ea49e7d1cfc95e73dbc96f3952
Blake Abel <blake.abel@dowjones.com>
2019-06-10 17:03:37 -0400

    Adding multiple site screenshotting in

--- example-screenshot.js
+++ example-screenshot.js
@@ -1,10 +1,17 @@
 const puppeteer = require('puppeteer');
+const sites = require("./sites.json");
+
  
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://barrons.com');
-  await page.screenshot({path: 'example.png', fullPage: true});
+  
+  for(i = 0; i < sites.length; i++){
+    let filename = 'example' + i + '.png'
+    await page.goto(sites[i]);
+    await page.screenshot({path: filename, fullPage: true});
+    console.log(i);
+  }
  
   await browser.close();
 })();
--- handler.js
+++ handler.js
@@ -1,6 +1,34 @@
 'use strict';
+const puppeteer = require('puppeteer');
+const sites = require("./sites.json");
 
-module.exports.hello = async (event) => {
+
+module.exports.screenshot = async (event) => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  for(i = 0; i < sites.length; i++){
+    let filename = 'example' + i + '.png'
+    await page.goto(sites[i]);
+    await page.screenshot({path: filename, fullPage: true});
+  }
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Screenshots taken! Yay.',
+      input: event,
+    }, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.archiveFiles = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
