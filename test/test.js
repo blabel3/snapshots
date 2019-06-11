1281a79f9da5ced9be6e0708a54e720b6a47c837
Blake Abel <blake.abel@dowjones.com>
2019-06-11 16:39:11 -0400

    Added placeholder tests for when I can access S3

--- test/test.js
+++ test/test.js
@@ -4,15 +4,48 @@ const control = require('./control');
 
 describe('Screenshots', function() {
     describe('Takes screenshot', function(){
-        it('can reach all of the websites in sites.json', async function() {
-            this.timeout(0); //Perhaps adjust after we know a ballpark for the sites/
+        it('Reaches all of the websites in sites.json', async function() {
+            this.timeout(0); //Perhaps adjust after we know a ballpark for the sites instead of disabling
             let reachable = await screenshots.goToSites();
             assert.equal(reachable, control.reachable);
         });
         
-        it('can take a screenshot of a webpage', async function() {
+        it('Takes a screenshot of a webpage', async function() {
             let screenshot = await screenshots.screenCapture();
             assert.equal(screenshot, control.screenshot);
+        });
+    });
+    //TODO: Write tests for storage.
+    describe('Stores screenshot', function(){
+        it('Accesses storage S3 bucket', function(){
+
+        });
+
+        it('Uploads to storage S3 bucket', function() {
+
+        });
+    });
+});
+
+//TODO: Write tests for File getter.
+describe('Resources (HTML/CSS/JS)', function(){
+    describe("Gets files from website's S3 bucket", function() {
+        it('Accesses S3 bucket', function(){
+
+        });
+
+        it('Gets files from S3 Bucket', function(){
+
+        });
+    });
+
+    describe("Puts files into storage S3 bucket", function(){
+        it('Accesses storage S3 bucket', function(){
+
+        });
+
+        it('Puts files into storage S3 bucket', function(){
+
         });
     });
 });
