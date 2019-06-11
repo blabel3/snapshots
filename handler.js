d42730edb38651f407828c9c90afa3a8ac01c8c4
Blake Abel <blake.abel@dowjones.com>
2019-06-11 16:58:24 -0400

    Small changes to prep for S3 access

--- handler.js
+++ handler.js
@@ -1,21 +1,24 @@
 'use strict';
+
 const puppeteer = require('puppeteer');
 const sites = require('./sites');
 
-
 module.exports.screenshot = async (event) => {
 
+  //Takes screenshot
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   for(i = 0; i < sites.length; i++){
     let filename = 'example' + i + '.png'
     await page.goto(sites[i]);
-    await page.screenshot({path: filename, fullPage: true});
+    await page.screenshot({path: filename, fullPage: true}); //should probably keep as buffer instead
     process.stdout.write(`${i}... `);
   }
 
   await browser.close();
+
+  //Stores them
 
   return {
     statusCode: 200,
