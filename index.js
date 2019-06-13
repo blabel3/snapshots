cb11ad6d92a907e3e99f68a077ff94ac9bd8feb8
Blake Abel <blake.abel@dowjones.com>
2019-06-13 11:57:40 -0400

    Better setup for working with S3 buckets

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 const puppeteer = require('puppeteer');
 const aws = require('aws-sdk');
 const sites = require('./sites');
+const files = require('./files');
 
-let s3 = new aws.S3();
+//Set up all AWS services we need to access from here
+let s3 = new aws.S3({
+  apiVersion: '2006-03-01', //latest as of 2019-06-13, but don't want to use latest in case anything changes.
+  region: 'us-east-1', 
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 module.exports.handler = async (event) => {
 
@@ -19,7 +26,7 @@ module.exports.handler = async (event) => {
     process.stdout.write(`${i}... `);
     
     //Stores them in s3
-    let filename = "Barrons" + sites[i].substring(23);
+    let filename = "Barrons" + sites[i].substring(23) + "/screenshots"; // ex. Barrons/penta/screenshots
 
     let screenshotStoreParams = {
       Body: screenshot,
@@ -37,34 +44,28 @@ module.exports.handler = async (event) => {
 
   await browser.close();
 
-  
-
-  process.env.SAVE_BUCKET;
-  process.env.FETCH_BUCKET;
-
   //Gets CSS/HTML/JS 
 
-  let fetchParams = {
-    Bucket: process.env.FETCH_BUCKET,
-    Key: "Filename.blahblah"
-  }
+  for(let filename in files){
 
-  s3.getObject(fetchParams, function(error, data) {
-    if (error) console.error(error);
-    else {
-      //Object recieved!
-      //Store object in storage bucket
+    let fetchParams = {
+      Bucket: process.env.FETCH_BUCKET,
+      Key: filename
+    }
 
-      //find all objects to put in storage
-      for (blah in data){
-        //store the objects
-        let screenshotStoreParams = {
-          Body: websiteResource,
+    s3.getObject(fetchParams, function(error, resource) {
+      if (error) console.error(error);
+      else {
+        //Object recieved!
+        //Store object in storage bucket
+
+        let resourceStoreParams = {
+          Body: resource,
           Bucket: process.env.SAVE_BUCKET,
-          Key: filename //need to come up with these?
+          Key: filename //need to edit these?
         }
     
-        s3.putObject(screenshotStoreParams, function(error, data){
+        s3.putObject(resourceStoreParams, function(error, data){
           if (error) console.error(error); 
           else {
             console.log(data); //Will be stuff like the Etag and the versionID. 
@@ -72,8 +73,8 @@ module.exports.handler = async (event) => {
         })
 
       }
+    })
 
-    }
-  })
+  }
 
 };
