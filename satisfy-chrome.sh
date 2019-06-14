266a76bdc724ba9428af11b7fb604b6be1cded33
Blake Abel <blake.abel@dowjones.com>
2019-06-14 15:51:28 -0400

    Added hopefully all the other dependencies we need

--- satisfy-chrome.sh
+++ satisfy-chrome.sh
@@ -4,7 +4,7 @@ ls
 
 ldd chrome | grep not
 
-sudo yum -y install cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango
+sudo yum -y install cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango gtk3 gdk-pixbuf2 atk at-spi2-core at-spi2-atk alsa-lib  libXcomposite libXi libXtst libXScrnSaver 
 
 ldd chrome | grep not
 
