74bb32a7e8f929c071b39bbc4be0180e0fedef80
Blake Abel <blake.abel@dowjones.com>
2019-06-19 14:40:30 -0400

    Getting site files from defcon-1 via http request

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 'use strict';
 
 //Internal dependencies
-const sites = require('./data/sites');
-const files = require('./data/files');
+const paths = require('./data/paths');
 
 //External dependencies
+const http = require('http');
 const puppeteer = require('puppeteer');
 const aws = require('aws-sdk');
 const express = require('express');
+const axios = require('axios');
+//let axios = 'yay'
 
 //Initilization
 const app = express();
 const saveBucket = process.env.LOCAL ? "test-bucket" : process.env.SERVO_S3_BUCKET;
+
+const host = "https://www.barrons.com";
 
 //Set up all AWS services we need to access from here
 let config = {
@@ -21,7 +25,7 @@ let config = {
   secretAccessKey: process.env.SERVO_S3_SECRET_KEY ? process.env.SERVO_S3_SECRET_KEY : 'S3RVER'
 }
 
-if(process.env.LOCAL) config.endpoint = "http://localhost:4568";
+if(process.env.LOCAL) config.endpoint = "http://localhost:9001";
 
 let s3 = new aws.S3(config);
 
@@ -43,16 +47,14 @@ app.get('/_health', (req, res) => {
 // TODO: Work with express better!
 app.get('/', async (req, res) => {
 
-  console.log(saveBucket);
-  console.log(process.env.LOCAL);
-  console.log(config);
+  let dateAppend;
 
   //Takes screenshot
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
-  for(let i = 0; i < sites.length; i++){
-    await page.goto(sites[i]);
+  for(let i = 0; i < paths.length; i++){
+    await page.goto(host + paths[i]);
     let screenshot = await page.screenshot({fullPage: true});
     process.stdout.write(`${i}... `);
 
@@ -60,8 +62,8 @@ app.get('/', async (req, res) => {
     
     //Stores them in s3
     var d = new Date();
-    var dateAppend = d.getUTCFullYear() + "/" + d.getUTCMonth() + "/" + d.getUTCDate();
-    let filename = "Barrons" + sites[i].substring(23) + "/screenshots/" + dateAppend + ".png"; // ex. Barrons/penta/screenshots
+    dateAppend = d.getUTCFullYear() + "/" + d.getUTCMonth() + "/" + d.getUTCDate();
+    let filename = "Barrons/" + dateAppend + paths[i] + "screenshots/" + "shot" + ".png"; // ex. Barrons/penta/screenshots
 
     let screenshotStoreParams = {
       Body: screenshot,
@@ -81,42 +83,46 @@ app.get('/', async (req, res) => {
 
   await browser.close();
 
-  res.send('<p>Done!</p>');
-
   //Gets CSS/HTML/JS 
 
-  /*for(let file in files){
+  let requests = [];
 
-    let fetchParams = {
-      Bucket: process.env.FETCH_BUCKET,
-      Key: file
+  //Set up all requests we need
+  for(let i = 0; i < paths.length; i++){
+
+    console.log(paths[i])
+
+    let request = axios.get((host + paths[i]), {
+      headers: { 'CF-CACHE-TAG': process.env.CF_CACHE_TAG }
+    });
+
+    requests.push(request);
+
+  }
+
+  //Run all requests simultaneously then work with the responses.
+  Promise.all(requests).then( (responses) => {
+
+    for(let i = 0; i < responses.length; i++){
+      console.log(responses[i].status);
+
+      let resourceStoreParams = {
+        Body: responses[i].data,
+        Bucket: saveBucket,
+        Key: "Barrons/" + dateAppend  + paths[i] + "resources/" + "index.html" //need to edit these?
+      }
+
+      s3.putObject(resourceStoreParams, function(error, data){
+        if (error) console.error(error); 
+        else {
+          console.log(data); //Will be stuff like the Etag and the versionID. 
+        }
+      })
     }
 
-    s3.getObject(fetchParams, function(error, resource) {
-      if (error) console.error(error);
-      else {
-        //Object recieved!
-        //Store object in storage bucket
+  }).then(() => { res.send('<p>Done!</p>') });
 
-        let resourceStoreParams = {
-          Body: resource,
-          Bucket: saveBucket,
-          Key: file //need to edit these?
-        }
-    
-        s3.putObject(resourceStoreParams, function(error, data){
-          if (error) console.error(error); 
-          else {
-            console.log(data); //Will be stuff like the Etag and the versionID. 
-          }
-        })
-
-      }
-    })
-
-  }*/
-
-})
+});
 
 //Binding to servo specified port
 app.listen(process.env.PORT, () => {
