62ca091fe96c526fb07fa7223c9922b60272ae58
Blake Abel <blake.abel@dowjones.com>
2019-07-12 12:44:23 -0400

    Added color changing for background with products, and started drop down fixes

--- public/main.js
+++ public/main.js
@@ -1,17 +1,38 @@
-let datepicker = $('#archive').datepicker({
-    language: "en",
-    inline: true,
-    minDate: new Date(2019, 7 - 1, 8),
-    maxDate: new Date(),
-    dateFormat: "d m yyyy",
-    onRenderCell: function (date, cellType) {
-        if (cellType == 'day') {
-            return (date.getDay() == 0 || date.getDay() == 6) ? {disabled: true} : { disabled: false }
+$(document).ready( () => {
+    let datepicker = $('#archive').datepicker({
+        language: "en",
+        inline: true,
+        minDate: new Date(2019, 7 - 1, 8), //year, month (adjusted for 0-11 instead of 1-12), and day from first snap.
+        maxDate: new Date(),
+        dateFormat: "d m yyyy",
+        onRenderCell: function (date, cellType) {
+            if (cellType == 'day') {
+                return (date.getDay() == 0 || date.getDay() == 6) ? {disabled: true} : { disabled: false }
+            }
+        },
+        onSelect: function updateDropdown(formattedDate, date, inst){
+            console.log(formattedDate);
+            let dateArray = formattedDate.split(" ").map( numberString => parseInt(numberString));
+            /*$("#pages").empty();
+            $("#breakpoints").empty();
+            $.get(`pages/${dateArray[0]}/${dateArray[1]}/${dateArray[2]}`, (data) => {
+                console.log(data);
+            });*/
+        }
+    }).data('datepicker');
+
+    datepicker.selectDate(new Date()); 
+
+    let products = $('input[name=product]');
+    console.log(products);
+    for (let i = 0; i < products.length; i++){
+        products[i].onchange = function () {
+            let label = $(`label[for="${products[i].id}"`);
+            let color = window.getComputedStyle(label[0]).getPropertyValue('border-top-color');
+            $("body").css("background-color", color);
         }
     }
-}).data('datepicker');
-
-datepicker.selectDate(new Date());
+});
 
 function downloadDate() {
     let dirtyDate = document.getElementById("archive").value;
@@ -23,4 +44,7 @@ function downloadDate() {
 function downloadScreenshot() {
     let page = document.getElementById("pages").value;
     console.log(page);
+
 }
+
+
