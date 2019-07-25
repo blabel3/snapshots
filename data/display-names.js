5aad3fd361d73717c71ba222872ee5fc76330b26
Blake Abel <blake.abel@dowjones.com>
2019-07-25 18:24:56 -0400

    Linted display-names

--- data/display-names.js
+++ data/display-names.js
@@ -1,15 +1,15 @@
 const getDisplayName = product => {
-    switch (product) {
-        case 'barrons':
-        return 'Barrons'
-        case 'wsj':
-        return 'WSJ'
-        case 'fnlondon':
-        return 'FNLondon'
-        default:
-        console.log('No display name set, assuming you want it the same.')
-    }
-    return product
+  switch (product) {
+    case 'barrons':
+      return 'Barrons'
+    case 'wsj':
+      return 'WSJ'
+    case 'fnlondon':
+      return 'FNLondon'
+    default:
+      console.log('No display name set, assuming you want it the same.')
+  }
+  return product
 }
 
 module.exports.getDisplayName = getDisplayName
